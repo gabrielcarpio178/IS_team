d9fcb0e8c28cb75c27321bc6a2403cfcfaea4d77
unknown <gabrielcarpio178@gmail.com>
2023-12-29 10:54:13 +0800

    12/30/2023 10:54AM

---
+++ js/payment.js
@@ -0,0 +1,12 @@
+const boxes = document.querySelectorAll('.boxes');
+window.addEventListener('scroll', scrollcheck);
+scrollcheck();
+function scrollcheck(){
+    const triggerBottom = window.innerHeight / 5 * 4;
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top;
+        if(boxTop<triggerBottom){
+            box.classList.add("animate__fadeInUp");
+        }
+    })
+}
